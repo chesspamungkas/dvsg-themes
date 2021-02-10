e87643f3f8c8b05ca760db2a930e03a2725400dc
Steve Khong <43911106+dv-stevekhong@users.noreply.github.com>
2021-02-10 20:50:39 +0800

    debug author page

--- author.php
+++ author.php
@@ -23,8 +23,8 @@ get_header();
             <div class="container" id="author-header">
                 <div class="row no-gutters">
                     <div class="col-12 text-center">
-                        <h1 class="eb-garamond-regular" id="author-title"><?php echo get_the_author_meta( 'display_name', get_the_author_meta('ID') ); ?></h1>
-                        <?php echo get_the_author_meta( 'description', get_the_author_meta('ID') ); ?>
+                        <h1 class="eb-garamond-regular" id="author-title"><?php echo get_the_author(); ?></h1>
+                        <?php echo get_the_author_meta( 'description' ); ?>
                     </div>
                 </div>
             </div>
