066de9613c14156859a062b763224ea44156923f
chesspamungkas <chesspamungkas@gmail.com>
2021-04-16 15:04:24 +0700

    Minify Javascript and Css jquery-ui

--- src/core/BaseStyle.php
+++ src/core/BaseStyle.php
@@ -5,13 +5,13 @@ class BaseStyle {
   private static $_registeredStyle = [
     'fontawesome-css'=>['https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css'],
     'fonts'=>['https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'],
-    'jquery-ui-css'=>['https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css']    
+    'jquery-ui-css'=>['https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.min.css']    
   ];
 
   private static $_registeredScript = [
     'boot2'=>['https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', 'jquery', true],
     'boot3'=>['https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', 'jquery', true],
-    'jquery-ui'=>['https://code.jquery.com/ui/1.12.1/jquery-ui.js', 'jquery', true]
+    'jquery-ui'=>['https://code.jquery.com/ui/1.12.1/jquery-ui.min.js', 'jquery', true]
   ];
   static function init() {
     self::AddScript('DV_coreScript', get_template_directory_uri().'/src/.dist/index.js', 'jquery', true);
