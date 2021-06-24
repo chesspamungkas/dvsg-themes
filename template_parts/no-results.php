8e3dced0a3017f9de964933e93b44c1abe5604fb
Steve Khong <43911106+dv-stevekhong@users.noreply.github.com>
2021-06-24 11:30:33 +0800

    Update no-results.php

--- template_parts/no-results.php
+++ template_parts/no-results.php
@@ -19,9 +19,23 @@
     margin-left: auto;
     margin-right: auto;
 }
+
+.no-result-background {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background-image: url( 'https://uploads.dailyvanity.sg/wp-content/uploads/2021/06/404.jpg' ); 
+    background-size: contain; 
+    background-repeat: no-repeat; 
+    background-position: bottom left;
+    z-index: 0;
+    opacity: 0.75;
+}
 </style>
 
-<div class="entry" style="min-height:400px; background-image: url( 'https://uploads.dailyvanity.sg/wp-content/uploads/2021/06/404.jpg' ); background-size: contain; background-repeat: no-repeat; background-position: bottom left; text-align:center;">
+<div class="entry" style="min-height:400px; text-align:center; position: relative;">
 <!--If no results are found-->
 	<h1>Oh no! This page seems to be broken just like the eyeshadows! :(</h1>
 	<p style="font-size: 26px;">You can browse our website via the links below, search for the content you want in the search bar above or report this issue to us at <a href="mailto:only@dailyvanity.sg">only@dailyvanity.sg</a>.</p>
@@ -32,5 +46,6 @@
 			<!--/div-->
 		</div>
 	</div>
+    <div class="no-result-background"></div>
 </div>
 <!--End if no results are found-->
