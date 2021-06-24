b6f26fde7df0534aa1a01f1cbc7cb9c09573c4e1
Steve Khong <43911106+dv-stevekhong@users.noreply.github.com>
2021-06-24 10:00:31 +0800

    Update no-results.php

--- template_parts/no-results.php
+++ template_parts/no-results.php
@@ -23,7 +23,7 @@
 
 <div class="entry" style="min-height:400px; background-image: url( 'https://uploads.dailyvanity.sg/wp-content/uploads/2021/06/404.jpg' ); background-size: contain; background-repeat: no-repeat; background-position: bottom left; text-align:center;">
 <!--If no results are found-->
-	<h1>Oh no! This page seems to be broken just like the eyeshadows!</h1>
+	<h1>Oh no! This page seems to be broken just like the eyeshadows! :(</h1>
 	<p style="font-size: 26px;">You can browse our website via the links below, search for the content you want in the search bar above or report this issue to us at <a href="mailto:only@dailyvanity.sg">only@dailyvanity.sg</a>.</p>
 	<div class="container">
 		<div class="row p-0 m-0">
