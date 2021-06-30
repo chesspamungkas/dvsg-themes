d002f46dcd8947a887c51e42d4e9b0fe912c5f20
Steve Khong <43911106+dv-stevekhong@users.noreply.github.com>
2021-06-30 21:09:37 +0800

    Update no-results.php

--- template_parts/no-results.php
+++ template_parts/no-results.php
@@ -42,8 +42,7 @@
 
 <div class="entry no-result-entry" style="min-height:400px; text-align:center; position: relative;">
 <!--If no results are found-->
-	<h1>Oh no! This page seems to be broken just like the eyeshadows! :(</h1>
-	<p style="font-size: 26px;">You can browse our website via the links below, search for the content you want in the search bar above or report this issue to us at <a href="mailto:only@dailyvanity.sg">only@dailyvanity.sg</a>.</p>
+	<?php echo do_action( '404-content' ); ?>
 	<div class="container" id="no-result">
 		<div class="row p-0 m-0">
 			<!--div class="col m-0 p-5"-->
