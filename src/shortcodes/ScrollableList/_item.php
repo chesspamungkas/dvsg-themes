4474a0b21e7f3a852aed784478140072a44f3312
Chris Sim <monkeymon@gmail.com>
2022-03-16 14:33:26 +0800

    DVBA core update

--- src/shortcodes/ScrollableList/_item.php
+++ src/shortcodes/ScrollableList/_item.php
@@ -7,14 +7,14 @@
       <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>" class="card-img-top" alt="<?php echo get_the_post_thumbnail_caption($post->ID) ?>">
     </a>
     <div class="card-body px-0">
-      <?php do_action(ScrollableList::$SCROLLABLE_LIST_BEFORE_ITEM_TITLE) ?>
+      <?php do_action(ScrollableList::$SCROLLABLE_LIST_BEFORE_ITEM_TITLE, $post) ?>
       <h5 class="card-title"><a href="<?php the_permalink(); ?>" class="poppins-medium" target="_blank" tabindex="0"><?php the_title(); ?></a></h5>
-      <?php do_action(ScrollableList::$SCROLLABLE_LIST_AFTER_ITEM_TITLE) ?>
-      <?php do_action(ScrollableList::$SCROLLABLE_LIST_BEFORE_ITEM_LINK) ?>
+      <?php do_action(ScrollableList::$SCROLLABLE_LIST_AFTER_ITEM_TITLE, $post) ?>
+      <?php do_action(ScrollableList::$SCROLLABLE_LIST_BEFORE_ITEM_LINK, $post) ?>
       <div class="more-detail-container">
         <a href="<?php the_permalink(); ?>" class="poppins-medium more-details-btn" target="_blank" tabindex="0"><?php echo apply_filters(ScrollableList::$SCROLLABLE_ITEM_VIEW_MORE_BTN_TEXT, 'More Details') ?></a>
       </div>
-      <?php do_action(ScrollableList::$SCROLLABLE_LIST_AFTER_ITEM_LINK) ?>
+      <?php do_action(ScrollableList::$SCROLLABLE_LIST_AFTER_ITEM_LINK, $post) ?>
     </div>
   </div>
 </div>
